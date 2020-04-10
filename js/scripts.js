14f1625e59e6be6a64a707bff2d05ef0398d56e0
Aaron Deimund <aaron@deimunddesigns.com>
2020-04-10 16:17:06 -0500

    working on the mobile version created mixins for breakpoints

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   const navButton = document.getElementById("primary-navigation-button");
+  const navMenu = document.getElementById("primary-navigation")
   navButton.addEventListener("click", () => {
     navButton.classList.toggle("change");
+    navMenu.classList.toggle("hide-right");
+    navMenu.classList.toggle("show-right");
   });
   console.log("working");
 });
